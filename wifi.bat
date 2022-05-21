0e946f963258a9afeafe9710133367d3eea48aa5
Kingron <kingron.lu@gmail.com>
2022-05-21 13:46:14 +0800

    Update wifi.bat

--- wifi.bat
+++ wifi.bat
@@ -1,147 +1,226 @@
-@echo off
-@title WIFI Sharing Tools
+<!-- : Begin batch script
+@ECHO OFF
+SETLOCAL EnableExtensions EnableDelayedExpansion
+TITLE WiFi Sharing Menu: Fools Edition
+COLOR 02
+ECHO.
 
-REM WIFI Sharing Tools for Windows
-REM Copyright Kingron<kingron@163.com>
-REM Only test for Windows 7(English version, Simplified Chinese version)
-REM This tools can create virtual WIFI access point, start and stop virtual WLAN,
-REM The virtual WLAN AP can be used for any mobile device, smart phone and etc.
-REM You WIFI adapter must support Ad-Hoc mode(For intel WLAN chipset, it means
-REM Intel MyWiFi), most of the wifi chipset support Ad-Hoc mode now.
-REM You should see a "Microsoft Virtual WiFi Miniport Adapter" from "control panel\
-REM network and internet\network connections"
-REM !WARNNING!
-REM    You MUST run this batch with administrator permissions
+	REM WiFi Sharing Menu for Windows
+	REM Requires Administrator permissions
+	REM Only tested on Windows 7 & 10(English version)
+	REM This tool can create, start and stop a virtual WiFi access point.
+	REM The virtual WLAN AP can be used with any mobile device, etc.
+	REM Your WIFI adapter must support Ad-Hoc mode(Intel MyWiFi), most support it.
+	REM "Microsoft Virtual WiFi Miniport Adapter" will show in Network Connections
+	REM ^(Run ncpa.cpl in a run/command prompt)
 
-set help=0
-if "%1"=="/?" set help=1
-if "%1"=="help" set help=1
-if "%1"=="-help" set help=1
-if %help% equ 1 (
-  echo WIFI sharing tools v1.1
-  echo Usage
-  echo    %~n0 [create ^| start ^| stop ^| view ^| password ^| help]
-  exit /b 0
-)
+	REM Check for admin permissions
+	NET FILE >NUL 2>&1
+	IF NOT "%ERRORLEVEL%" == "0" (
+		ECHO Administrator permission is required^!
+		ECHO Please click [Yes] on the UAC dialog.
+		TIMEOUT 3
+		cscript //nologo "%~f0?.wsf" //job:Admin
+		GOTO :EXIT
+	)
 
-net session >nul 2>&1
-if not "%errorLevel%" == "0" (
-  echo Oops: This tools must run with administrator permissions!
-  echo it will popup the UAC dialog, please click [Yes] to continue.
-  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-  echo UAC.ShellExecute "%~s0", "%*", "", "runas", 1 >> "%temp%\getadmin.vbs"
+	REM Display help
+	IF "%~1" == "" GOTO :MENU
+	CALL :%~1 2>NUL
+	IF "%ERRORLEVEL%" == "1" (
+		ECHO Syntax: %~nx0 [Option]
+		ECHO    [create, start, stop, view, password, help]
+		ECHO.
 
-  "%temp%\getadmin.vbs"
-  exit /b 2
-)
+		ECHO Copyright (C) 2013 Kingron <kingron@163.com>
+		ECHO Modified by Fooly Cooly
+		ECHO Licensed with GPL v3 https://www.gnu.org/licenses/gpl-3.0.txt
+		ECHO.
+	)
+	PAUSE
+	GOTO :EXIT
 
-if "%1"=="create" goto create
-if "%1"=="start" goto start
-if "%1"=="stop" goto stop
-if "%1"=="view" goto view
-if "%1"=="password" goto password
-if "%1"=="share" goto share
+	:MENU
+	REM Show WiFi Sharing Menu
+	CLS
+	ECHO.
+	ECHO   â”Œâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”
+	ECHO   â”‚     WiFi Sharing Menu      â”‚
+	ECHO   â”œâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”¤
+	ECHO   â”‚  1. Create Virtual WLAN    â”‚
+	ECHO   â”‚  2. Start Virtual WLAN     â”‚
+	ECHO   â”‚  3. Stop Virtual WLAN      â”‚
+	ECHO   â”‚  4. View WLAN Connections  â”‚
+	ECHO   â”‚  5. Change WLAN Name       â”‚
+	ECHO   â”‚  6. Change WLAN Password   â”‚
+	ECHO   â”‚  7. Share Connection(ICS)  â”‚
+	ECHO   â”‚  8. Exit                   â”‚
+	ECHO   â””â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”˜
+	ECHO.
 
-:menu
-echo.
-echo ©³©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©·
-echo ©§     WIFI Tools Menu      ©§
-echo ©Ç©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©Ï
-echo ©§ 1. Create virtual WLAN   ©§
-echo ©§ 2. Start virtual WLAN    ©§
-echo ©§ 3. Stop virtual WLAN     ©§
-echo ©§ 4. View WLAN connections ©§
-echo ©§ 5. Change WLAN password  ©§
-echo ©§ 6. Share Connection(ICS) ©§
-echo ©§ 7. Exit                  ©§
-echo ©»©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¥©¿
-echo.
-set /p mid=Please select the number and press ^<ENTER^>:
-if "%mid%"=="1" goto create
-if "%mid%"=="2" goto start
-if "%mid%"=="3" goto stop
-if "%mid%"=="4" goto view
-if "%mid%"=="5" goto password
-if "%mid%"=="6" goto share
-if "%mid%"=="7" goto end
-echo Error: Invalid command, please try again.
-goto menu
+	REM If calling a label fails ERRORLEVEL is set to 1 and the below message appears
+	IF "%ERRORLEVEL%" == "1" ECHO Error: Invalid command, please try again.
 
-:create
-echo.
-echo NOTE:
-echo The "create virtual WLAN" command only run once if success, you needn't run it
-echo again unless you want to change the SSID or password!
-echo.
+	REM Clear the value from last selection
+	CALL SET SLC=
 
-REM if you want to use this for other language, you should change below tags.
-REM CP 936 = Chinese, 437 = English
-echo Check your WIFI adapter...
-set supported=0
-netsh wlan show drive | find "Ö§³ÖµÄ³ĞÔØÍøÂç" | find "ÊÇ"
-if %errorlevel%==0 set supported=1
-netsh wlan show drive | find "Hosted network supported" | find "Yes"
-if %errorlevel%==0 set supported=1
-if %supported% equ 1 (
-  echo Congratulation! You WIFI adapter support Ad-Hoc mode.
-  echo Please follow step to finish the setup.
-) else (
-  echo Oops! You WIFI adapter can't support Ad-Hoc mode^(hostednetwork^).
-  exit /b 1
-)
+	REM Prompt user for input
+	SET /p SLC=Select a number and press ^<ENTER^>:
 
-if "%_name%"=="" set _name=wlan
-set /p _name=Please input the virtual AP name(default: %_name%):
-set /p _password=Please input the password^(required, length: 8~63^):
-netsh wlan set hostednetwork mode=allow ssid=%_name% key=%_password%
-if "%errorlevel%"=="0" (
-  echo Setup the WLAN success.
-)
-netsh wlan start hostednetwork
-if "%errorlevel%"=="0" (
-  echo Startup WLAN success, enjoy it!
-  echo Please goto control panel, network connections, share the internet connection
-  echo to virtual WIFI adapter.
-) else (
-  echo Error: Started WLAN failure.
-)
-goto end
+	REM Call user chosen label, pause and reshow menu
+	CALL :%SLC% 2>NUL
+	IF "%SLC%" == "8" GOTO :EXIT
+	PAUSE
+	GOTO :MENU
 
-:start
-netsh wlan start hostednetwork
-if "%errorlevel%"=="0" (
-  echo Startup WLAN success, enjoy it!
-) else (
-  echo Error: Started WLAN failure.
-)
-goto end
+	:1
+	:CREATE
+	SETLOCAL
+		ECHO.
+		ECHO Checking Ad-Hoc mode support...
 
-:stop
-netsh wlan stop hostednetwork
-goto end
+		REM Get current language code page
+		FOR /F "tokens=2 delims=:." %%A IN ('CHCP') DO SET CP=%%A
 
-:password
-set /p _password=Please input the password^(required, length: 8~63^):
-netsh wlan set hostednetwork key=%_password% > nul
-if "%errorlevel%"=="0" (
-  echo Change WLAN password success!
-) else (
-  echo Error: Change WLAN password failure.
-  echo Please check inputed password and try again.
-  goto menu
-)
-goto end
+		REM CP 437 = English
+		IF %CP% == 437 NETSH wlan show drive | find "Hosted network supported" | find "Yes"
 
-:view
-netsh wlan show hostednetwork
-goto end
+		REM CP 936 = Chinese, 
+		IF %CP% == 936 NETSH wlan show drive | find "æ”¯æŒçš„æ‰¿è½½ç½‘ç»œ" | find "æ˜¯"
 
-:share
-cscript /nologo %~dp0\share.vbs
-goto end
+		ECHO.
+		IF "%ERRORLEVEL%" == "0" (
+			REM Choose access point name or default
+			SET /p _name=Please input virtual AP name:
+			IF "%_name%" == "" SET "_name=WiFi Hotspot" & ECHO Name defaulted to !_name!
 
-:end
-set _name=
-set _password=
-set mid=
-if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+			REM Choose access point password or default
+			SET /p _password=Please input password^(required, length: 8~63^):
+			IF "%_password%" == "" SET "_password=password" & ECHO Password defaulted to !_password!
+			ECHO.
+
+			REM Set access point settings
+			NETSH wlan set hostednetwork mode=allow ssid="!_name!" key="!_password!"
+			IF "%ERRORLEVEL%" == "0" ECHO WLAN Setup Successful
+
+			REM Start access point
+			NETSH wlan start hostednetwork > NUL
+			IF "%ERRORLEVEL%" == "0" (
+				ECHO WLAN Startup Successful!
+				ECHO.
+				ECHO NOTE:
+				ECHO   Only run "Create virtual WLAN" command once if successful.
+				ECHO   You needn't run it again unless you want to change the name or password!
+				ECHO   Please share an internet connection with virtual WiFi adapter.
+			) ELSE ( ECHO Error: WLAN Startup Failed )
+
+		) ELSE ( ECHO Error: Your WiFi adapter doesn't support Ad-Hoc mode^(hostednetwork^) )
+	ENDLOCAL
+	GOTO :EOF
+
+	:2
+	:START
+		REM Start WiFi AP and check if it errored
+		NETSH wlan start hostednetwork
+		IF "%ERRORLEVEL%"=="0" (
+			ECHO WLAN startup success, enjoy it!
+		) ELSE ( ECHO Error: WLAN startup failed )
+		GOTO :EOF
+
+	:3
+	:STOP
+		REM Stop WiFi Access Point
+		NETSH wlan stop hostednetwork
+		GOTO :EOF
+
+	:4
+	:VIEW
+		REM Show WiFi Access Points
+		NETSH wlan show hostednetwork
+		GOTO :EOF
+
+	:5
+	:SSID
+		SET /p _name=Please input new name^(required, length: 8~63^):
+		NETSH wlan set hostednetwork ssid=%_name% > nul
+		IF NOT "%ERRORLEVEL%" == "0" (
+			ECHO Error: WLAN name change failed. Please try again.
+		) ELSE ( ECHO WLAN name change success! )
+		GOTO :EOF
+
+	:6
+	:PASSWORD
+		REM Prompt user for new password and set it
+		SET /p _password=Please input new password^(required, length: 8~63^):
+		NETSH wlan set hostednetwork key=%_password% > nul
+		IF NOT "%ERRORLEVEL%" == "0" (
+			ECHO Error: WLAN password change failed. Please try again.
+		) ELSE ( ECHO WLAN password change success! )
+		GOTO :EOF
+
+	:7
+	:SHARE
+		REM Runs the internal vbscript to share connections
+		cscript //nologo "%~f0?.wsf" //job:Share
+		GOTO :EOF
+
+:EXIT
+REM Clean up of settings
+ENDLOCAL
+TITLE Command Prompt
+COLOR 7
+CLS
+EXIT /B
+
+----- Begin wsf script --->
+<package>
+  <job id="Admin">
+    <script language="VBScript">
+		File = Left(WScript.ScriptFullName, Len(WScript.ScriptFullName) -5)
+		Set UAC = CreateObject("Shell.Application")
+		UAC.ShellExecute "cmd", "/c " & File, "", "runas", 1
+	</script>
+  </job>
+  <job id="Share">
+    <script language="VBScript">
+		dim pub, prv, idx
+
+		ICSSC_DEFAULT         = 0
+		CONNECTION_PUBLIC     = 0
+		CONNECTION_PRIVATE    = 1
+		CONNECTION_ALL        = 2
+
+		set NetSharingManager = Wscript.CreateObject("HNetCfg.HNetShare.1")
+
+		wscript.echo "No.   Name" & vbCRLF & "------------------------------------------------------------------"
+		idx = 0
+		set Connections = NetSharingManager.EnumEveryConnection
+		for each Item in Connections
+			idx = idx + 1
+			set Connection = NetSharingManager.INetSharingConfigurationForINetConnection(Item)
+			set Props = NetSharingManager.NetConnectionProps(Item)
+			szMsg = CStr(idx) & "     " & Props.Name
+			wscript.echo szMsg
+		next
+		wscript.echo "------------------------------------------------------------------"
+		wscript.stdout.write "Select public connection(for internet access) No.: "
+		pub = cint(wscript.stdin.readline)
+		wscript.stdout.write "Select private connection(for share users) No.: "
+		prv = cint(wscript.stdin.readline)
+		if pub = prv then
+		  wscript.echo "Error: Public can't be same as private!"
+		  wscript.quit
+		end if
+
+		idx = 0
+		set Connections = NetSharingManager.EnumEveryConnection
+		for each Item in Connections
+			idx = idx + 1
+			set Connection = NetSharingManager.INetSharingConfigurationForINetConnection(Item)
+			set Props = NetSharingManager.NetConnectionProps(Item)
+			if idx = prv then Connection.EnableSharing CONNECTION_PRIVATE
+			if idx = pub then Connection.EnableSharing CONNECTION_PUBLIC
+		next
+	</script>
+  </job>
+</package>
